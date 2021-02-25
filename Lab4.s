7fbed52098341c41587018714fd94fd4f0639bee
andy051200 <bon19451@uvg.edu.gt>
2021-02-25 01:12:36 -0600

    Update Lab4.s
    ya me cuadró la sección del 7seg automático, solo falta configurar la freq correcta

--- Lab4.s
+++ Lab4.s
@@ -13,7 +13,7 @@ PROCESSOR 16F887
     
 ;----------------------bits de configuraciÃ³n-----------------------------------
 ;------configuration word 1----------------------------------------------------
-CONFIG  FOSC=INTRC_NOCLKOUT ;se declara osc interno 
+CONFIG  FOSC=INTRC_NOCLKOUT ;se declara osc interno
 CONFIG  WDTE=OFF            ; Watchdog Timer apagado
 CONFIG  PWRTE=ON            ; Power-up Timer prendido
 CONFIG  MCLRE=OFF           ; MCLRE apagado
@@ -33,7 +33,7 @@ CONFIG WRT = OFF	    ;apagado de auto escritura de cÃƒÂ³digo
 ;---------------------- configuración de macros -------------------------------   
 reset_timer	macro	    ; lo que anteriormente fue subrutina, se hizo macro
     banksel	PORTA	    ; nos aseguramos que es el PortA
-    movlw	61	    ; dada la configuración del prescaler
+    movlw	81	    ; dada la configuración del prescaler
     movwf	TMR0	    ; se guarda en timer0
     bsf		T0IF	    ; bandera cuando no hay overflow
     endm     
@@ -56,20 +56,21 @@ PSECT intVect, class=CODE, abs, delta=2 ; ubicación de resetVector 2bytes
 ;--------------- vector de interrupciones -------------------------------------
 ORG 04h		     ; ubicación inicial de interrupción
 push:				   
-    movwf	W_TEMP		; variable se almacena en en 
-    swapf	STATUS, W	    ; se cambian bits para no subir banderas
-    movf	STATUS_TEMP	    ; se mueve a variable
+    movwf	W_TEMP		; variable se almacena en f
+    swapf	STATUS, W	; se dan vuelta nibble
+    movf	STATUS_TEMP	; se almacena en status_temp
     
 isr:
     btfsc	T0IF		    ; se evalua si se hizo la interrupción
     call	interruption_tm0    ; se llama a la interrupción
     btfsc	RBIF		    ; se evalua si se hizo la interrupción  
-    call	interruption_oc	    ; se llama a rutina de interrupcion
+    call	interruption_oc	    ; 
+    call	suma_portd
     
 pop:
-    swapf	STATUS_TEMP, W 
-    movwf	STATUS
-    swapf	W_TEMP, F
+    swapf	STATUS_TEMP, W	    ; se da vuelta a variable anterior
+    movwf	STATUS		    ; se almacena en status
+    swapf	W_TEMP, F	    ; se da vuelta a variable anterior
     swapf	W_TEMP, W
     retfie	
      
@@ -77,7 +78,7 @@ pop:
 interruption_tm0:
     reset_timer		; solo de 50ms
     incf	cont
-    bcf		T0IF		    ; bajar bandera para que salg de la interrupcion
+    bcf		T0IF	; bajar bandera para que salg de la interrupcion
     return
 
 interruption_oc:
@@ -88,7 +89,19 @@ interruption_oc:
     decf	PORTA
     bcf		RBIF
     return
-    
+
+suma_portd:
+    reset_timer		; solo de 50ms
+    incf	cont
+    movf	cont, W
+    sublw	128
+    btfss	STATUS, 2
+    goto	return_timer0
+    clrf	cont
+    incf	PORTD
+    bsf		T0IF
+return_timer0:
+    return
 ;---------------------- configuración de programa -----------------------------
 PSECT code, delta=2, abs    ; se ubica el cÃ³digo de 2 bytes
 ORG 100h    
@@ -98,18 +111,18 @@ ORG 100h
     bsf	    PCLATH, 0 	    ; 
     andlw   0fh		    ; se eliminan los 4 MSB y se dejan los 4 LSB
     addwf   PCL, F	    ; se guarda en F
-    retlw   00111111B	    ; 0
-    retlw   00000110B	    ; 1
-    retlw   01011011B	    ; 2
-    retlw   01001111B	    ; 3
-    retlw   01100110B	    ; 4
-    retlw   01101101B	    ; 5 
-    retlw   01111101B	    ; 6
-    retlw   00000111B	    ; 7
-    retlw   01111111B	    ; 8
-    retlw   01101111B	    ; 9
+    retlw   0x3F	    ; 0
+    retlw   0x06	    ; 1
+    retlw   0x5B	    ; 2
+    retlw   0x4F	    ; 3
+    retlw   0x66	    ; 4
+    retlw   0x6D	    ; 5 
+    retlw   0x7D	    ; 6
+    retlw   0x07	    ; 7
+    retlw   0x7F	    ; 8
+    retlw   0x6F	    ; 9
     retlw   01110111B	    ; A
-    retlw   01111100B	    ; B
+    retlw   01111111B	    ; B
     retlw   00111001B	    ; C
     retlw   01011110B	    ; D
     retlw   01111001B	    ; E
@@ -119,12 +132,12 @@ main:
     call	io_config	; rutina de configuración in/out
     call	reloj_config	; rutina de configuración de reloj
     call	timer_config	; rutina de configuración de relok
-    call	interruption_config ; rutina de config de interrumpciones
+    call	interruption_config	; rutina de configuración de interrumpciones
     banksel	PORTA
           
 ;-------------------- loop principal de programa ------------------------------
 loop:
-    call	inc_cont
+    ; de momento no hay loops
     goto	loop
         
 ;-------------------- subrutinas de programa ----------------------------------
@@ -137,13 +150,8 @@ io_config:
     clrf	TRISA	    ; PortA se configura como salida
     bsf		TRISB, 0    ; entraba RB0
     bsf		TRISB, 7    ; entrada RB7
-    
-    banksel	TRISC	    ;
-    /*bcf		TRISC	    ; salida de PortC
-    bcf		PORTC
-    bcf		TRISD	    ; salida de PortD
-    bcf		PORTD*/
-    
+    clrf	TRISC	    ; portC como salida
+    clrf	TRISD	    ; portD como salida
     
     bcf		OPTION_REG, 7  ; set
     bsf		WPUB, 0	    ; weak pull-up PortB, 0
@@ -154,22 +162,24 @@ io_config:
     
     banksel	PORTA	    ; selección
     clrf	PORTA	    ; PortA se configura como salida
-    clrf	PORTB	    ; 
+    clrf	PORTB	    ; PortB como salida
+    clrf	PORTC	    ; PortC como salida
+    clrf	PORTD	    ; PortD como salida
     return
     
 reloj_config:
     banksel	OSCCON
-    bsf		IRCF2	    ; set, configuraciÃ³n de frecuencia a 4MkHz (110)
-    bsf		IRCF1	    ; set, configuraciÃ³n de frecuencia a 4MHz (110)
-    bcf		IRCF0	    ; clear, configuraciÃ³n de frecuencia a 4MHz (110)
+    bcf		IRCF2	    ; set, configuraciÃ³n de frecuencia a 4MkHz (110)
+    bcf		IRCF1	    ; set, configuraciÃ³n de frecuencia a 4MHz (110)
+    bsf		IRCF0	    ; clear, configuraciÃ³n de frecuencia a 4MHz (110)
     return
 
 timer_config:
     banksel	TRISC	    ; asegurarse de estar en el banco
     bcf		T0CS	    ; Internal instruction cycle clock = 0
     bcf		PSA	    ; estos PS son el preescaler
-    bcf		PS2	    ; prescaler 001
-    bcf		PS1	    ; prescaler 001
+    bsf		PS2	    ; prescaler 001
+    bsf		PS1	    ; prescaler 001
     bsf		PS0	    ; prescaler 00  1 
     reset_timer		    ; se reinicia el contador
     return
@@ -180,58 +190,6 @@ interruption_config:
     bcf		T0IF	    ; desactivar Timer0 overflow interruption flag
     bsf		RBIE
     bcf		RBIF
-    return
-
-suma:
-    btfsc	PORTB, 0
-    goto	$-1	    ; regresar una linea en cÃ³digo
-    incf	PORTA
-    return
-
-resta:
-    btfsc	PORTB, 7
-    goto	$-1	    ; regresar una linea en cÃ³digo
-    decf	PORTA	    ; 
-    return
-
-display:
-    movlw	00001111B	    ; se pone limite
-    andwf	cont, F	    ; pone limite de los bits y almacena en F
-    movf	cont, W	    ; se almacena en W
-    call	tabla	    ; se toma el valor dentro de tabla
-    movwf	PORTC	    ; valor que tenga tabla se manda a PortC
-    return
-
-inc_cont:
-    movf	cont, W
-    sublw	40	    ; practicamente se multiplica x10
-    btfss	ZERO ;STATUS, 2 bit ZERO
-    goto	$+5
-    clrf	cont
-    incf	segmentos	
-    movf	segmentos, W
-    movwf	PORTD
-    return
-
-contador:
-    btfss	T0IF		; skip if set cuando se prenda T0IF
-    goto	$-1		; loop si pasa o no
-    call	reset_timer	; amonos reiniciando timer
-    incf	PORTB
-    movlw	00001111B	; se pone limite a contador
-    andwf	PORTB, F	; pone limite de los bits 
-    return
-   
-/*resta:
-    btfsc	PORTB, 7
-    goto	$-1	    ; regresar una linea en cÃ³digo
-    decf	cont	    ; 
-    movlw	00001111B   ; se pone limite
-    andwf	cont, F	    ; pone limite de los bits
-    movf	cont, W
-    call	tabla
-    movwf	PORTA
-    return*/
-    
+    return   
     
 END      
